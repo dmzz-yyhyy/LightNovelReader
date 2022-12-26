867682492e20bf2c4b6bb1643c093c73e11ee50d
dmzz-yyhyy <198580666@outlook.com>
2022-12-26 12:47:53 +0800

    Initial commit

--- app/src/main/java/indi/dmzzyyhyy/lightnovelreader/MainActivity.kt
+++ app/src/main/java/indi/dmzzyyhyy/lightnovelreader/MainActivity.kt
@@ -2,10 +2,30 @@ package indi.dmzzyyhyy.lightnovelreader
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+                val navController = navHostFragment.navController
+                when (tab) {
+                    tabLayout.getTabAt(0) -> navController.navigate(R.id.homeFragment)
+                    tabLayout.getTabAt(1) -> navController.navigate(R.id.bookshelfFragment)
+                    tabLayout.getTabAt(2) -> navController.navigate(R.id.mineFragment)
+                }
+            }
+
+            override fun onTabReselected(p0: TabLayout.Tab?) {}
+            override fun onTabUnselected(p0: TabLayout.Tab?) {}
+        })
     }
 }
---
+++ app/src/main/java/indi/dmzzyyhyy/lightnovelreader/ui/HomeFragment.kt
@@ -0,0 +1,20 @@
+package indi.dmzzyyhyy.lightnovelreader.ui
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+import indi.dmzzyyhyy.lightnovelreader.R
+
+class HomeFragment : Fragment() {
+    @SuppressLint("InflateParams")
+    @Nullable
+    @Override
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_home, null)
+        return view
+    }
+}
