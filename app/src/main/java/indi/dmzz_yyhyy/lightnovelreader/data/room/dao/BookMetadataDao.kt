45196b057af7e7e8719a824349d9abe6f44c8d63
dmzz-yyhyy <hk198580666@outlook.com>
2023-08-18 18:58:07 +0800

    v0.3.2

--- app/src/main/java/indi/dmzz_yyhyy/lightnovelreader/data/room/dao/BookMetadataDao.kt
+++ app/src/main/java/indi/dmzz_yyhyy/lightnovelreader/data/room/dao/BookMetadataDao.kt
@@ -1,15 +1,7 @@
 package indi.dmzz_yyhyy.lightnovelreader.data.room.dao
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
 
 @Dao
 abstract class BookMetadataDao {
-    @Query("SELECT * FROM bookmetadata WHERE bookId=:bookId")
-    abstract suspend fun get(bookId: Int): BookMetadataDao
-    @Insert
-    abstract suspend fun add(book: BookMetadataDao)
-    @Query("DELETE FROM bookmetadata WHERE bookId=:bookId")
-    abstract suspend fun delete(bookId: Int)
 }
