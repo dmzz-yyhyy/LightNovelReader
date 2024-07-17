0cecde29786ddef73b27f915a0c3e412f0e673e1
NightFish <101402767+dmzz-yyhyy@users.noreply.github.com>
2024-07-18 00:15:30 +0800

    修改ViewModel

--- app/src/main/java/indi/dmzz_yyhyy/lightnovelreader/ui/book/detail/DetailViewModel.kt
+++ app/src/main/java/indi/dmzz_yyhyy/lightnovelreader/ui/book/detail/DetailViewModel.kt
@@ -1,7 +1,10 @@
 package indi.dmzz_yyhyy.lightnovelreader.ui.book.detail
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class DetailViewModel {
+class DetailViewModel @Inject constructor(
+) : ViewModel() {
 }
--- app/src/main/java/indi/dmzz_yyhyy/lightnovelreader/ui/home/reading/ReadingViewModel.kt
+++ app/src/main/java/indi/dmzz_yyhyy/lightnovelreader/ui/home/reading/ReadingViewModel.kt
@@ -1,8 +1,12 @@
 package indi.dmzz_yyhyy.lightnovelreader.ui.home.reading;
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import javax.inject.Inject;
 
 @HiltViewModel
-public class ReadingViewModel {
+class ReadingViewModel @Inject constructor(
+
+) : ViewModel() {
 
 }
