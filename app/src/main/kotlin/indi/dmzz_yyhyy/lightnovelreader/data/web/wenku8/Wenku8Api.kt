ee161634de3ef124b8729f36c43b16b6d3b847c8
NightFish <101402767+dmzz-yyhyy@users.noreply.github.com>
2024-08-05 21:08:18 +0800

    修正书本类容获取格式化

--- app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/data/web/wenku8/Wenku8Api.kt
+++ app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/data/web/wenku8/Wenku8Api.kt
@@ -11,9 +11,7 @@ import indi.dmzz_yyhyy.lightnovelreader.data.web.exploration.ExplorationPageData
 import indi.dmzz_yyhyy.lightnovelreader.utils.wenku8.wenku8Api
 import java.net.URLEncoder
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import kotlin.io.encoding.ExperimentalEncodingApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
@@ -24,7 +22,6 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 
 object Wenku8Api: WebBookDataSource {
     private var coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO)
@@ -37,8 +34,6 @@ object Wenku8Api: WebBookDataSource {
             delay(2500)
         }
     }
-    private var isStopSearch = false
-    private var runningSearchCount = 0
 
     private fun isOffLine(): Boolean {
         try {
@@ -51,7 +46,6 @@ object Wenku8Api: WebBookDataSource {
 
     override suspend fun getIsOffLineFlow(): Flow<Boolean> = isOffLineFlow
 
-    @OptIn(ExperimentalEncodingApi::class)
     override suspend fun getBookInformation(id: Int): BookInformation {
         if (isOffLine()) return BookInformation.empty()
         return wenku8Api("action=book&do=meta&aid=$id&t=0").let {
@@ -64,7 +58,7 @@ object Wenku8Api: WebBookDataSource {
                 tags = it.selectFirst("[name=Tags]")?.attr("value")?.split(" ") ?: emptyList(),
                 publishingHouse = it.selectFirst("[name=PressId]")?.attr("value") ?: "",
                 wordCount = it.selectFirst("[name=BookLength]")?.attr("value")?.toInt() ?: -1,
-                lastUpdated = LocalDate.parse(it.selectFirst("[name=LastUpdate]")?.attr("value")).atStartOfDay(),
+                lastUpdated = LocalDate.parse(it.selectFirst("[name=LastUpdate]")?.attr("value"), DATA_TIME_FORMATTER).atStartOfDay(),
                 isComplete = it.selectFirst("[name=BookStatus]")?.attr("value") == "已完成"
             )
         }
@@ -105,18 +99,30 @@ object Wenku8Api: WebBookDataSource {
         return wenku8Api("action=book&do=text&aid=$bookId&cid=$chapterId&t=0")
             .let { document ->
                 document
-                    .toString()
-                    .replace("<html><head></head><body>\n\n", "")
-                    .replace("\n \n</body></html>", "")
+                    .wholeText()
                     .let { s ->
-                        println(allBookChapterListCache)
+                        var title = ""
+                        var content = ""
+                        s.split("\n").forEachIndexed { index, line ->
+                            if (content != "") return@forEachIndexed
+                            if (title == "" && line.any { !it.isWhitespace() }) {
+                                title = line.trim()
+                                return@forEachIndexed
+                            }
+                            if (title != "" && line.any { !it.isWhitespace() }) {
+                                content = s.split("\n").drop(index).joinToString("\n")
+                                return@forEachIndexed
+                            }
+                        }
+                        val images = Regex("(<!--image-->)(.*?)(<!--image-->)")
+                            .findAll(document.toString())
+                            .map { it.groups[2]?.value ?: "" }
+                            .toList()
                         ChapterContent(
                             id = chapterId,
-                            title = s.split("\n\n \n  \n \n\n \n    \n    \n    \n \n\n\n \n \n")[0],
-                            content = s
-                                .split("\n\n \n  \n \n\n \n    \n    \n    \n \n\n\n \n \n")[1]
-                                .replace("<!--image-->", "[image]")
-                                .replace("       <!--image-->", "\n[image]")
+                            title = title,
+                            content =
+                                if (images.isEmpty()) content else images.joinToString { "[image]$it[image]" }.replace(", ", "")
                             ,
                             lastChapter = allBookChapterListCache
                                 .indexOfFirst { it.id == chapterId }
@@ -141,44 +147,6 @@ object Wenku8Api: WebBookDataSource {
         )
 
     override suspend fun getExplorationPageTitleList(): List<String> = listOf("首页", "全部", "分类")
-
-    private fun getSearchResult(document: Document): List<BookInformation> = document
-            .select("#content > table > tbody > tr > td > div")
-            .map { element ->
-                BookInformation(
-                    id = element.selectFirst("div > div:nth-child(1) > a")
-                        ?.attr("href")
-                        ?.replace("/book/", "")
-                        ?.replace(".htm", "")
-                        ?.toInt() ?: -1,
-                    title = element.selectFirst("div > div:nth-child(1) > a")
-                        ?.attr("title") ?: "",
-                    coverUrl = element.selectFirst("div > div:nth-child(1) > a > img")
-                        ?.attr("src") ?: "",
-                    author = element.selectFirst("div > div:nth-child(2) > p:nth-child(2)")
-                        ?.text()?.split("/")?.getOrNull(0)
-                        ?.split(":")?.getOrNull(1) ?: "",
-                    description = element.selectFirst("div > div:nth-child(2) > p:nth-child(5)")
-                        ?.text()?.replace("简介:", "") ?: "",
-                    tags = emptyList(),
-                    publishingHouse = element.selectFirst("div > div:nth-child(2) > p:nth-child(2)")
-                        ?.text()?.split("/")?.getOrNull(1)
-                        ?.split(":")?.getOrNull(1) ?: "",
-                    wordCount = element.selectFirst("div > div:nth-child(2) > p:nth-child(3)")
-                        ?.text()?.split("/")?.getOrNull(1)
-                        ?.split(":")?.getOrNull(1)
-                        ?.replace("K", "")?.toInt()?.times(1000) ?: -1,
-                    lastUpdated = element.selectFirst("div > div:nth-child(2) > p:nth-child(3)")
-                        ?.text()?.split("/")?.getOrNull(0)
-                        ?.split(":")?.getOrNull(1)
-                        ?.let {
-                            LocalDate.parse(it, DATA_TIME_FORMATTER)
-                        }
-                        ?.atStartOfDay() ?: LocalDateTime.MIN,
-                    isComplete = element.selectFirst("div > div:nth-child(2) > p:nth-child(3)")
-                        ?.text()?.split("/")?.getOrNull(2) == "已完结"
-                )
-            }
 
     override fun search(searchType: String, keyword: String): Flow<List<BookInformation>> {
         val searchResult = MutableStateFlow(emptyList<BookInformation>())
