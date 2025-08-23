0704835c7e80ae3621780d66f8289743e731c6bb
Gaurav Yadav <gauravyad2077@gmail.com>
2025-08-23 17:08:53 +0545

    fix the navigation issue

--- app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/extensions/home/extensionsHomeNavigation.kt
+++ app/src/main/kotlin/indi/dmzz_yyhyy/lightnovelreader/ui/extensions/home/extensionsHomeNavigation.kt
@@ -4,20 +4,22 @@ import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import indi.dmzz_yyhyy.lightnovelreader.ui.LocalNavController
 import indi.dmzz_yyhyy.lightnovelreader.ui.navigation.Route
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 fun NavGraphBuilder.extensionsHomeNavigation(sharedTransitionScope: SharedTransitionScope) {
     composable<Route.Main.Extensions.Home> {
+        val navController = LocalNavController.current
         ExtensionsHomeScreen(
             onNavigateToRepositories = {
-                // Navigation will be handled by parent
+                navController.navigate(Route.Main.Extensions.Repositories)
             },
             onNavigateToBrowse = {
-                // Navigation will be handled by parent
+                navController.navigate(Route.Main.Extensions.Browse)
             },
             onNavigateToInstalled = {
-                // Navigation will be handled by parent
+                navController.navigate(Route.Main.Extensions.Installed)
             }
         )
     }
