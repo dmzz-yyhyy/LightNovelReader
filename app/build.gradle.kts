97fb16299d9643059a04e65d1c282ca28dca6e7c
Justhuayu <justhuayu@qq.com>
2026-03-31 20:36:45 +0800

    修复 GihubParser.kt 解析失败，导致无法检查更新问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 24
         targetSdk = 36
         // 版本号为x.y.z则versionCode为x*1000000+y*10000+z*1000+debug版本号(开发需要时迭代, 三位数)
-        versionCode = 1_02_00_034
+        versionCode = 1_02_00_035
         versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
